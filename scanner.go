ca45b0e29c3712c79d91d7a10fcf1b598841593c
Chris Gilling <cgilling@iparadigms.com>
2015-02-09 21:59:11 -0800

    delete unneeded commented out code

--- scanner.go
+++ scanner.go
@@ -72,32 +72,6 @@ func NewFastScanner(r io.Reader) *Scanner {
 	return s
 }
 
-/*
-// dropCR drops a terminal \r from the data.
-func dropCR(data []byte) []byte {
-	if len(data) > 0 && data[len(data)-1] == '\r' {
-		return data[0 : len(data)-1]
-	}
-	return data
-}
-
-func modifiedScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	if i := bytes.IndexByte(data, '\n'); i >= 0 {
-		// We have a full newline-terminated line.
-		return i + 1, dropCR(data[0:i]), nil
-	}
-	// If we're at EOF, we have a final, non-terminated line. Return it.
-	if atEOF {
-		return len(data), dropCR(data), nil
-	}
-	// Request more data.
-	return 0, nil, nil
-}
-*/
-
 func (s *Scanner) Scan() bool {
 	if s.fastMode {
 		return s.fastScan()
