c61a92cdc2fc5dc5fce6eda484c0d0db7ed37d67
Chris Gilling <cgilling@iparadigms.com>
2015-02-14 10:54:56 -0800

    remove build subcommand

--- cppdep/main.go
+++ cppdep/main.go
@@ -52,95 +52,93 @@ func (c *Config) ReadFile(path string) error {
 }
 
 func main() {
-	rootCmd := cli.App("cppdep", "dependency graph and easy compiles")
-	configPath := rootCmd.StringOpt("config", "", "path to yaml config")
-	rootCmd.Command("build", "build binaries from a given directory", func(cmd *cli.Cmd) {
-		cmd.Spec = "[--concurrency] [--fast] [--regex] SRCDIR [BINARY_NAME]"
-		srcDir := cmd.StringArg("SRCDIR", "./", "")
-		binaryName := cmd.StringArg("BINARY_NAME", "", "name of the binary to build, main source file should be BINARY_NAME.cc")
-		concurrency := cmd.IntOpt("c concurrency", 1, "How much concurrency to we want to allow")
-		fast := cmd.BoolOpt("fast", false, "Set to enable fast file scanning")
-		regex := cmd.BoolOpt("regex", false, "Treat binaryName as a path regex")
+	cmd := cli.App("cppdep", "dependency graph and easy compiles")
+	cmd.Spec = "[OPTIONS] SRCDIR [BINARY_NAME]"
+	configPath := cmd.StringOpt("config", "", "path to yaml config")
+	concurrency := cmd.IntOpt("c concurrency", 1, "How much concurrency to we want to allow")
+	fast := cmd.BoolOpt("fast", false, "Set to enable fast file scanning")
+	regex := cmd.BoolOpt("regex", false, "Treat binaryName as a path regex")
+	srcDir := cmd.StringArg("SRCDIR", "./", "")
+	binaryName := cmd.StringArg("BINARY_NAME", "", "name of the binary to build, main source file should be BINARY_NAME.cc")
 
-		cmd.Action = func() {
-			fmt.Printf("config: %q, srcDir: %q\n", *configPath, *srcDir)
-			config := &Config{}
-			if *configPath != "" {
-				if err := config.ReadFile(*configPath); err != nil {
-					log.Fatalf("Failed to read config file: %q", err)
-				}
+	cmd.Action = func() {
+		fmt.Printf("config: %q, srcDir: %q\n", *configPath, *srcDir)
+		config := &Config{}
+		if *configPath != "" {
+			if err := config.ReadFile(*configPath); err != nil {
+				log.Fatalf("Failed to read config file: %q", err)
 			}
-			if config.BuildDir == "" {
-				log.Fatalf("BuildDir must be set")
-			}
-			err := os.MkdirAll(config.BuildDir, 0755)
-			if err != nil {
-				log.Fatalf("Failed to create build dir: %s (%v)", config.BuildDir, err)
-			}
-
-			var gens []cppdep.Generator
-			for _, gen := range config.TypeGenerators {
-				gens = append(gens, &cppdep.TypeGenerator{
-					InputExt:   gen.InputExt,
-					OutputExts: gen.OutputExts,
-					Command:    gen.Command,
-				})
-			}
-			for _, gen := range config.ShellGenerators {
-				gens = append(gens, &cppdep.ShellGenerator{
-					InputPaths:    gen.InputPaths,
-					OutputFiles:   gen.OutputFiles,
-					ShellFilePath: filepath.Join(*srcDir, gen.Path),
-				})
-			}
-
-			if config.BuildDir, err = filepath.Abs(config.BuildDir); err != nil {
-				log.Fatalf("Failed to get absolute path of build dir")
-			}
-
-			st := &cppdep.SourceTree{
-				SrcRoot:         *srcDir,
-				IncludeDirs:     config.Includes,
-				ExcludeDirs:     config.Excludes,
-				Libraries:       config.Libraries,
-				SourceLibs:      config.SourceLibs,
-				Concurrency:     *concurrency,
-				UseFastScanning: *fast,
-				Generators:      gens,
-				BuildDir:        config.BuildDir,
-			}
-			if err := st.ProcessDirectory(); err != nil {
-				log.Fatalf("Failed to process source directory: %s (%v)", *srcDir, err)
-			}
-
-			c := &cppdep.Compiler{
-				OutputDir:   config.BuildDir,
-				IncludeDirs: append(config.Includes, st.GenDir()),
-				Flags:       config.Flags,
-				Concurrency: *concurrency,
-			}
-			var files []*cppdep.File
-			if *binaryName == "" {
-				files, err = st.FindMainFiles()
-				if err != nil {
-					log.Fatalf("failes to automatically find main files: %v", err)
-				}
-			} else if *regex {
-				files, err = st.FindSources(*binaryName + ".cc")
-				if err != nil {
-					log.Fatalf("invalid regex: %q", *binaryName)
-				}
-			} else {
-				mainFile := st.FindSource(*binaryName + ".cc")
-				if mainFile == nil {
-					log.Fatalf("Unable to find source for %q", *binaryName)
-				}
-				files = append(files, mainFile)
-			}
-
-			c.CompileAll(files)
 		}
-	})
+		if config.BuildDir == "" {
+			log.Fatalf("BuildDir must be set")
+		}
+		err := os.MkdirAll(config.BuildDir, 0755)
+		if err != nil {
+			log.Fatalf("Failed to create build dir: %s (%v)", config.BuildDir, err)
+		}
 
-	rootCmd.Run(os.Args)
+		var gens []cppdep.Generator
+		for _, gen := range config.TypeGenerators {
+			gens = append(gens, &cppdep.TypeGenerator{
+				InputExt:   gen.InputExt,
+				OutputExts: gen.OutputExts,
+				Command:    gen.Command,
+			})
+		}
+		for _, gen := range config.ShellGenerators {
+			gens = append(gens, &cppdep.ShellGenerator{
+				InputPaths:    gen.InputPaths,
+				OutputFiles:   gen.OutputFiles,
+				ShellFilePath: filepath.Join(*srcDir, gen.Path),
+			})
+		}
+
+		if config.BuildDir, err = filepath.Abs(config.BuildDir); err != nil {
+			log.Fatalf("Failed to get absolute path of build dir")
+		}
+
+		st := &cppdep.SourceTree{
+			SrcRoot:         *srcDir,
+			IncludeDirs:     config.Includes,
+			ExcludeDirs:     config.Excludes,
+			Libraries:       config.Libraries,
+			SourceLibs:      config.SourceLibs,
+			Concurrency:     *concurrency,
+			UseFastScanning: *fast,
+			Generators:      gens,
+			BuildDir:        config.BuildDir,
+		}
+		if err := st.ProcessDirectory(); err != nil {
+			log.Fatalf("Failed to process source directory: %s (%v)", *srcDir, err)
+		}
+
+		c := &cppdep.Compiler{
+			OutputDir:   config.BuildDir,
+			IncludeDirs: append(config.Includes, st.GenDir()),
+			Flags:       config.Flags,
+			Concurrency: *concurrency,
+		}
+		var files []*cppdep.File
+		if *binaryName == "" {
+			files, err = st.FindMainFiles()
+			if err != nil {
+				log.Fatalf("failes to automatically find main files: %v", err)
+			}
+		} else if *regex {
+			files, err = st.FindSources(*binaryName + ".cc")
+			if err != nil {
+				log.Fatalf("invalid regex: %q", *binaryName)
+			}
+		} else {
+			mainFile := st.FindSource(*binaryName + ".cc")
+			if mainFile == nil {
+				log.Fatalf("Unable to find source for %q", *binaryName)
+			}
+			files = append(files, mainFile)
+		}
+
+		c.CompileAll(files)
+	}
+
+	cmd.Run(os.Args)
 }
